cf89aba4b7d79d90bbd360af8e043e66d7541242
cyruslognonne <cyrus7@hotmail.fr>
2017-12-30 13:32:52 -0500

    Adding the geo-location fetching stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,54 @@
 var socket = io();
 
-socket.on("connect", function() {
-    console.log("New connection from the server!");
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-socket.on("disconnect", function() {
-    console.log("Disctonnected from server");
-})
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
-
-socket.on("newMessage", function(message){
-  console.log("Message received from the server:", message);
-  var li = jQuery("<li></li>");
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
-  jQuery("#messages").append(li)
-})
 
-socket.on("newLocationMessage", function(message){
-  var li = jQuery("<li></li>");
-  var a = jQuery("<a target="_blank">My current Location</a>");
-  li.text(`${message.from}: `);
-  a.attr("href", message.url)
-  li.append(a);
-  jQuery("#messages").append(li);
+  jQuery('#messages').append(li);
 });
 
-jQuery("#message-form").on("submit", function(e) {
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  socket.emit("createMessage", {
-      from: "User",
-      text: jQuery("[name=message]").val()
-  }, function(){
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
 
-  })
-})
+  });
+});
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
 
-var locationButton = jQuery("#send-location");
-locationButton.on("click", function(){
-    if(!navigator.geolocation){
-      return alert("there's no geolocation!");
-    }
-
-  navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position);
-    socket.emit("createLocationMessage", {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-    })
-  }, function(){
-    alert("unable to fetch the location")
-  })
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
+  });
 });
--- server/server.js
+++ server/server.js
@@ -28,7 +28,6 @@ io.on("connection", (socket) => {
     })
 
     socket.on("createLocationMessage", (coords) =>{
-      io.emit("newMessage", generateMessage("Admin", `${coords.latitude}, ${coords.longitude}`));
       io.emit("newLocationMessage", generateLocationMessage("Admin", coords.latitude, coords.longitude));
     })
 
