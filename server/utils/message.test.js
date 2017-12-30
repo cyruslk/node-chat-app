5c390bc7f8be5fa15be83b2a94e3b0f2ebf96bdb
cyruslognonne <cyrus7@hotmail.fr>
2017-12-30 13:54:03 -0500

    Add geolocation supper

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require("expect");
-var {generateMessage} = require("./message");
+var {generateMessage, generateLocationMessage} = require("./message");
 
 
 describe("generateMessage", () => {
@@ -14,4 +14,21 @@ describe("generateMessage", () => {
     })
 })
 
-module.exports = {generateMessage};
+
+
+describe("generateLocationMessage", () => {
+    it("should generate correct location object", () => {
+      var from = "cyrus";
+      var latitude = 15;
+      var longitude = 15;
+      var url = "https://www.google.com/maps?q=15,15";
+
+      var message = generateLocationMessage(from, latitude, longitude);
+
+      expect(message.createdAt).toBeA("number");
+      expect(message).toInclude({from,url});
+
+    })
+})
+
+module.exports = {generateMessage, generateLocationMessage};
