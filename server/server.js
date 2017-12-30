a4ff5cdfad22dfaa8c49473da9490095d40185b1
cyruslognonne <cyrus7@hotmail.fr>
2017-12-30 10:34:06 -0500

    Adding coordonates pt.2

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on("connection", (socket) => {
     })
 
     socket.on("createLocationMessage", (coords) =>{
-      io.emit("newMessage", generateMessage("Admin", `${coords.latitude}, ${coords.latitude}`));
+      io.emit("newMessage", generateMessage("Admin", `${coords.latitude}, ${coords.longitude}`));
     })
 
   socket.on("disconnect", () => {
